998d757f856da4a574dc87e1f09cc056888bcef4
jimmy-james <mrjimbot@gmail.com>
2020-01-15 14:03:22 -0500

    feature: boilerplate for screens

---
+++ src/screens/AccountScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, StyleSheet, Text } from 'react-native'
+
+const AccountScreen = () => {
+    return <Text style={{ fontSize: 48 }}>ACCOUNT SCREEN</Text>
+}
+
+const styles = StyleSheet.create({})
+
+export default AccountScreen
---
+++ src/screens/TrackCreateScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, StyleSheet, Text } from 'react-native'
+
+const TrackCreateScreen = () => {
+    return <Text style={{ fontSize: 48 }}>TRACK CREATE SCREEN</Text>
+}
+
+const styles = StyleSheet.create({})
+
+export default TrackCreateScreen
