bd19c0769afbe7e5c85f4598a2fac3518ea4c70f
jimmy-james <mrjimbot@gmail.com>
2020-01-16 16:34:47 -0500

    feature: mocklocation and watch location

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,14 +1,42 @@
-import React from 'react'
+// this gets invoked here and expo-location will start emitting location changes every second.
+import '../_mockLocation'
+import React, { useEffect, useState } from 'react'
 import { StyleSheet } from 'react-native'
 import { Text } from 'react-native-elements'
 import { SafeAreaView } from 'react-navigation'
+import { requestPermissionsAsync, watchPositionAsync, Accuracy } from 'expo-location'
 import Map from '../components/Map'
 
 const TrackCreateScreen = () => {
+    const [err, setErr] = useState(null)
+
+    const startWatching = async() => {
+        try {
+            await requestPermissionsAsync()
+            // track user's location without moving around in development
+            // this watchPositionAsync is buggy, so we wrote the _mockLocation above for development testing
+            await watchPositionAsync({
+                accuracy: Accuracy.BestForNavigation,
+                timeInterval: 1000,
+                distanceInterval: 10,
+            }, (location) => {
+                // record this information on the map
+
+            })
+        } catch(e) {
+            setErr(e)
+        }
+    }
+
+    useEffect(() => {
+        startWatching()
+    }, [])
+
     return (
         <SafeAreaView forceInset={{ top: 'always' }}>
             <Text h3>Create a Track</Text>
             <Map />
+            { err ? <Text>Please enable location services</Text> : null }
         </SafeAreaView>
     )
 }
